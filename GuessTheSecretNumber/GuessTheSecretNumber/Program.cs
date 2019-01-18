b734fa65a788dde17b7986b622f9f2217de456a0
Maharoz <mahruj66@gmail.com>
2019-01-18 20:36:27 +0600

    guess the number(problem definition added)

--- GuessTheSecretNumber/GuessTheSecretNumber/Program.cs
+++ GuessTheSecretNumber/GuessTheSecretNumber/Program.cs
@@ -8,12 +8,16 @@ namespace GuessTheSecretNumber
 {
     class Program
     {
+
+        //problem definition  Write a program that picks a random number between 1 and 10.
+        //Give the user 4 chances to guess the number. If the user guesses the number, 
+        //display “You won"; otherwise, display “You lost".
         static void Main(string[] args)
         {
             var random = new Random();
             Console.WriteLine("Guess the number");
             var b = Convert.ToInt32(random.Next(1, 10));
-            Console.WriteLine(b);
+            //Console.WriteLine(b);
             for(int i = 0; i < 4; i++)
             {
                 var a = Convert.ToInt32(Console.ReadLine());

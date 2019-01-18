c274c5120e3a20a9a5efa3c52d81b6042d018f98
Maharoz <mahruj66@gmail.com>
2019-01-18 21:12:11 +0600

    finding maxium number from comma separated string

---
+++ MaximumFromCommaSeparated/MaximumFromCommaSeparated/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumFromCommaSeparated
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = 0;
+            Console.WriteLine("Please enter numbers in comma separated format");
+            var content = Console.ReadLine();
+            int[] numbers = content.Split(',').Select(int.Parse).ToList().ToArray();            
+            result = numbers[0];
+            for (int i = 0; i< numbers.Count(); i++)
+            {
+               
+                if(numbers[i]> result)
+                {
+                    result = numbers[i];
+
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}

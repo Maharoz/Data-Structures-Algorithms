a2d074d9f500db17584d5f9e61631677c7d31ac6
Maharoz <mahruj66@gmail.com>
2019-01-18 21:15:10 +0600

    find maximum from comma sepated ( problem definitin added)

--- MaximumFromCommaSeparated/MaximumFromCommaSeparated/Program.cs
+++ MaximumFromCommaSeparated/MaximumFromCommaSeparated/Program.cs
@@ -10,6 +10,10 @@ namespace MaximumFromCommaSeparated
     {
         static void Main(string[] args)
         {
+
+            //problem definition Write a program and ask the user to enter a series of 
+            //numbers separated by comma. Find the maximum of the numbers and display it 
+            //on the console. For example, if the user enters “5, 3, 8, 1, 4", the program should display 8
             var result = 0;
             Console.WriteLine("Please enter numbers in comma separated format");
             var content = Console.ReadLine();

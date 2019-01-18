c6ab32e5827f3cac43a976058802d7c00b5cf73a
Maharoz <mahruj66@gmail.com>
2019-01-18 18:41:57 +0600

    input a number or exit

---
+++ InputNumberOrExit/InputNumberOrExit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputNumberOrExit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //problem definition : Write a program and continuously ask
+            //the user to enter a number or "ok" to exit. Calculate the sum of 
+            //all the previously entered numbers and display it on the console.
+            bool a = true;
+            int number = 0;
+            while(a == true){
+                Console.WriteLine("Please enter a number");
+                var input = Console.ReadLine();
+                if (input.ToUpper() != "OK")
+                {
+                    number = number + Convert.ToInt32(input);
+                    
+                }
+                else
+                {
+                    a = false;
+                }
+            }
+            Console.WriteLine("Your total entered value is " + number);
+        }
+    }
+}

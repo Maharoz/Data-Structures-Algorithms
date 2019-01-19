651401eb455a778c2f5fad2c7d9b4228ddcc79ac
Maharoz <mahruj66@gmail.com>
2019-01-19 12:17:45 +0600

    inputting unique number and sorting them(problem definition added)

--- UniqueNumberSort/UniqueNumberSort/Program.cs
+++ UniqueNumberSort/UniqueNumberSort/Program.cs
@@ -10,6 +10,12 @@ namespace UniqueNumberSort
     {
         static void Main(string[] args)
         {
+            //problem definition Write a program and ask the user to enter 5 
+            //numbers.If a number has been previously entered, display an error
+            //message and ask the user to re-try. Once the user successfully enters 5 
+            //unique numbers, sort them and display the result on the console.
+
+
             var numbers = new List<int>();
 
             while (numbers.Count < 5)

46518754d565a35a669eaf48acd843dbae1ec339
Maharoz <mahruj66@gmail.com>
2019-01-20 21:54:56 +0600

    finding the largest element from an array

---
+++ LargestElementFinding/LargestElementFinding/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestElementFinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = new int[5] { 88,64,90,76,34};
+            var largest = number[0];
+            for(int i = 0; i < number.Length; i++)
+            {
+                if (largest < number[i])
+                {
+                    largest = number[i];
+                }
+            }
+            Console.WriteLine("Largest number is " + largest);
+        }
+    }
+}

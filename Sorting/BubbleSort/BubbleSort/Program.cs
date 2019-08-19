2407bcac36ac178243b782f4df8510388dfbd598
maharoz <mahruj66@gmail.com>
2019-08-19 21:52:24 +0600

    Bubble sort implemented

---
+++ Sorting/BubbleSort/BubbleSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CArray nums = new CArray(5);
+            Random rnd = new Random(100);
+            for (int i = 0; i < 5; i++)
+                nums.Insert((int)(rnd.NextDouble() * 100));
+            Console.WriteLine("Before sorting: ");
+            nums.DisplayElements();
+            Console.WriteLine("During sorting: ");
+            nums.BubbleSort();
+            Console.WriteLine("After sorting: ");
+            nums.DisplayElements();
+        }
+    }
+}

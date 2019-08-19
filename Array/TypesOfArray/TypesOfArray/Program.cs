2aa3441ef17d87f94f11abcd4631a9996bea87d9
maharoz <mahruj66@gmail.com>
2019-08-19 20:45:46 +0600

    Array Type Identified

---
+++ Array/TypesOfArray/TypesOfArray/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypesOfArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers;
+            numbers = new int[]{0,1,2,3,4};
+            Type arrType = numbers.GetType();
+            if (arrType.IsArray)
+            {
+                Console.WriteLine("The array type is : {0}",arrType);
+            }
+            else
+            {
+                Console.WriteLine("Not an array");
+            }
+
+            Console.Read();
+        }
+    }
+}

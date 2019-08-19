c8433a92caee818862860d30889ac8f2e67f504e
maharoz <mahruj66@gmail.com>
2019-08-19 20:37:28 +0600

    Array basics added

---
+++ ArrayBasics/ArrayBasics/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring an Array
+            string[] names = new string[10];
+
+            //setting values
+            names[2] = "Raymond";
+            names.SetValue("Maharoz",4);
+
+            //Geting Values
+            var myName = names[2];
+            var getName = names.GetValue(2);
+
+            Console.WriteLine(getName);
+        }
+    }
+}
--- ArrayBasics/ArrayBasics/Properties/AssemblyInfo.cs
+++ ArrayBasics/ArrayBasics/Properties/AssemblyInfo.cs
@@ -2,34 +2,34 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FindingTheOccuranceOfElement")]
+[assembly: AssemblyTitle("ArrayBasics")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FindingTheOccuranceOfElement")]
+[assembly: AssemblyProduct("ArrayBasics")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ed511562-a457-490c-9bc9-a457fecb9925")]
+[assembly: Guid("00c8b9b9-8198-4458-a1b2-43b34ec4336f")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]

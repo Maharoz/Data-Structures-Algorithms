dd8573e118fc1dca5a5646d908f39700349e1309
maharoz <mahruj66@gmail.com>
2019-09-19 23:46:43 +0600

    Electronic shop problem solved

---
+++ Hackerrank/Electronic Shop/Electronic Shop/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Electronic_Shop
+{
+    class Program
+    {
+        static int getMoneySpent(int[] keyboards, int[] drives, int totalMoneyAvailable)
+        {
+            var sortedKeyboardPrices = from keyboard in keyboards
+                orderby keyboard descending
+                select keyboard;
+            var sortedDrivePrices = from drive in drives
+                orderby drive ascending
+                select drive;
+            var maxMoneySpendable = -1;
+
+            foreach (var keyboard in sortedKeyboardPrices)
+            {
+                foreach (var drive in sortedDrivePrices)
+                {
+                    if (keyboard + drive <= totalMoneyAvailable)
+                    {
+                        if (keyboard + drive > maxMoneySpendable)
+                            maxMoneySpendable = keyboard + drive;
+                    }
+                    else
+                        break;
+                }
+            }
+            return maxMoneySpendable;
+        }
+
+        static void Main(String[] args)
+        {
+            string[] tokens_s = Console.ReadLine().Split(' ');
+            int s = Convert.ToInt32(tokens_s[0]);
+            string[] keyboards_temp = Console.ReadLine().Split(' ');
+            int[] keyboards = Array.ConvertAll(keyboards_temp, Int32.Parse);
+            string[] drives_temp = Console.ReadLine().Split(' ');
+            int[] drives = Array.ConvertAll(drives_temp, Int32.Parse);
+            int moneySpent = getMoneySpent(keyboards, drives, s);
+            Console.WriteLine(moneySpent);
+        }
+    }
+}

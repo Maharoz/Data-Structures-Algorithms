39f6f5db6fe1100a5f22467575c04224e22a052d
maharoz <mahruj66@gmail.com>
2019-09-23 22:15:26 +0600

    code for push notification

---
+++ PushNotificationXamarin/PushNotificationXamarin/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PushNotificationXamarin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var request = WebRequest.Create("https://onesignal.com/api/v1/notifications") as HttpWebRequest;
+
+            request.KeepAlive = true;
+            request.Method = "POST";
+            request.ContentType = "application/json; charset=utf-8";
+
+            byte[] byteArray = Encoding.UTF8.GetBytes("{"
+                                                      + "\"app_id\": \"b5f5a37e-f5e1-4663-b008-877dd5ad96d8\","
+                                                      + "\"contents\": {\"en\": \"Akash Message\"},"
+                                                      + "\"include_player_ids\": [\"88b30f7d-771b-4cf9-8088-0bb16eb03068\"]}");
+
+            string responseContent = null;
+
+            try
+            {
+                using (var writer = request.GetRequestStream())
+                {
+                    writer.Write(byteArray, 0, byteArray.Length);
+                }
+
+                using (var response = request.GetResponse() as HttpWebResponse)
+                {
+                    using (var reader = new StreamReader(response.GetResponseStream()))
+                    {
+                        responseContent = reader.ReadToEnd();
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                System.Diagnostics.Debug.WriteLine(ex.Message);
+                System.Diagnostics.Debug.WriteLine(new StreamReader(ex.Response.GetResponseStream()).ReadToEnd());
+            }
+
+            System.Diagnostics.Debug.WriteLine(responseContent);
+        }
+    }
+}

8df151da2b8a9a92d1373032bd1b2864e4b69703
maharoz <mahruj66@gmail.com>
2019-09-24 22:17:26 +0600

    Diagonal difference

---
+++ Hackerrank/Diagonal Difference/Diagonal Difference/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+
+namespace Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sumPrimaryDiagonal = 0;
+            var sumSecondaryDiagonal = 0;
+            var n = int.Parse(ReadLine());
+            for (int i = 0, j = n - 1; i < n; i++, j--)
+            {
+                var a_temp = ReadLine().Split(' ');
+                var newRow = Array.ConvertAll(a_temp, int.Parse);
+                sumPrimaryDiagonal += newRow[i];
+                sumSecondaryDiagonal += newRow[j];
+            }
+            WriteLine(Math.Abs(sumPrimaryDiagonal - sumSecondaryDiagonal));
+        }
+    }
+}

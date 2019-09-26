dfd5eb2dacaa524cb4b66f8256d5e86319c0b265
maharoz <mahruj66@gmail.com>
2019-09-26 21:45:49 +0600

    The hurdle race

---
+++ Hackerrank/The hurdle Race/The hurdle Race/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace The_hurdle_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine().Split(' ');
+            var s_a = Array.ConvertAll(s, int.Parse);
+
+            var v = Console.ReadLine().Split(' ');
+            var v_a = Array.ConvertAll(v, int.Parse);
+
+            int max_hurdle_height = 0;
+
+            for (int i = 0; i < v_a.Length; i++)
+            {
+                if (v_a[i] > max_hurdle_height)
+                {
+                    max_hurdle_height = v_a[i];
+                }
+            }
+
+            int result = max_hurdle_height - s_a[1];
+
+            if (result > 0)
+            {
+                Console.WriteLine(max_hurdle_height - s_a[1]);
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+        }
+    }
+}

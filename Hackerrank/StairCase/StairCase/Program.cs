ccc890603594cde2e256b3d379705d8ada6972ca
maharoz <mahruj66@gmail.com>
2019-09-29 23:17:43 +0600

    staircase

---
+++ Hackerrank/StairCase/StairCase/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+namespace StairCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                var spaces = new String(' ', n - i);
+                var hashes = new String('#', i);
+                WriteLine(spaces + hashes);
+
+            }
+
+        }
+    }
+}

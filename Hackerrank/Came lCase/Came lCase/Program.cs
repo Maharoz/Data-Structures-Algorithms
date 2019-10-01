7a83dec90543b0b6494fbec274bb2a1cf571cc50
maharoz <mahruj66@gmail.com>
2019-10-01 23:34:53 +0600

    Camel Case

---
+++ Hackerrank/Came lCase/Came lCase/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Came_lCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int word = 1;
+            foreach (char item in s)
+            {
+                if (item < 91 && item > 64)
+                {
+                    word += 1;
+                }
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
